0b35c5dd17b699c07494fd09115d1ca5d93d4d4f
Kilian Schulte <schultek@in.tum.de>
2022-12-16 12:34:01 +0100

    add schedules for cleaner and visitor

--- src/movement/schedule/Schedule.java
+++ src/movement/schedule/Schedule.java
@@ -11,6 +11,7 @@ import java.util.Random;
 public class Schedule {
 
     private static final int START_OF_DAY = 8;
+    private static final int END_OF_DAY = 20;
 
     private static final int NR_LECTURES_MIN = 1;
     private static final int NR_LECTURES_MAX = 3;
@@ -18,6 +19,25 @@ public class Schedule {
     // time in hours
     private static final int START_TIME_MIN = 8;
     private static final int START_TIME_MAX = 18;
+
+    private static final double FIVE_MINS = 1.0 / 12.0;
+    private static final double TEN_MINS = 1.0 / 6.0;
+    private static final double QUARTER_HOUR = 1.0 / 4.0;
+    private static final double TWENTY_MINS = 1.0 / 3.0;
+    private static final double HALF_HOUR = 1.0 / 2.0;
+
+
+    private static double STUDENT_PROB_EAT = 0.2;
+    private static double STUDENT_PROB_HANGOUT = 0.5;
+    private static double STUDENT_PROB_CHAIR = 0.3;
+
+    private static double PROF_PROB_EAT = 0.05;
+    private static double PROF_PROB_HANGOUT = 0.05;
+    private static double PROF_PROB_CHAIR = 0.9;
+
+    private static double VISITOR_PROB_EAT = 0.1;
+    private static double VISITOR_PROB_HANGOUT = 0.7;
+    private static double VISITOR_PROB_CHAIR = 0.2;
 
     ArrayList<ScheduleSlot> slots;
     RoomBase.RoomType exitRoom;
@@ -29,6 +49,24 @@ public class Schedule {
         lectures = new ArrayList<>();
 
         Random rng = new Random(s.getInt("RNG_SEED"));
+
+        STUDENT_PROB_EAT = s.getDouble("STUDENT_PROB_EAT", STUDENT_PROB_EAT);
+        STUDENT_PROB_HANGOUT = s.getDouble("STUDENT_PROB_HANGOUT", STUDENT_PROB_HANGOUT);
+        STUDENT_PROB_CHAIR = s.getDouble("STUDENT_PROB_CHAIR", STUDENT_PROB_CHAIR);
+
+        assert(STUDENT_PROB_EAT + STUDENT_PROB_CHAIR + STUDENT_PROB_HANGOUT == 1);
+
+        PROF_PROB_EAT = s.getDouble("PROF_PROB_EAT", PROF_PROB_EAT);
+        PROF_PROB_HANGOUT = s.getDouble("PROF_PROB_HANGOUT", PROF_PROB_HANGOUT);
+        PROF_PROB_CHAIR = s.getDouble("PROF_PROB_CHAIR", PROF_PROB_CHAIR);
+
+        assert(PROF_PROB_EAT + PROF_PROB_HANGOUT + PROF_PROB_CHAIR == 1);
+
+        VISITOR_PROB_EAT = s.getDouble("VISITOR_PROB_EAT", VISITOR_PROB_EAT);
+        VISITOR_PROB_HANGOUT = s.getDouble("VISITOR_PROB_HANGOUT", VISITOR_PROB_HANGOUT);
+        VISITOR_PROB_CHAIR = s.getDouble("VISITOR_PROB_CHAIR", VISITOR_PROB_CHAIR);
+
+        assert(PROF_PROB_EAT + PROF_PROB_HANGOUT + PROF_PROB_CHAIR == 1);
 
         int nrLectures = RoomBase.LectureRooms.size() * 4;
 
@@ -73,13 +111,18 @@ public class Schedule {
             return Schedule.forProfessor(seed);
         } else if (host.groupId.equals("barista")) {
             return Schedule.forBarista(seed);
+        } else if (host.groupId.equals("visitor")) {
+            return Schedule.forVisitor(seed);
+        } else if (host.groupId.equals("cleaner")) {
+            return Schedule.forCleaner(seed);
         } else {
-            return Schedule.empty();
+                return Schedule.empty();
+
         }
     }
 
     public static Schedule forStudent(int seed) {
-        Random rng = new Random(seed);
+        Random rng = randomForSeed(seed);
 
         ArrayList<ScheduleSlot> slots = new ArrayList<>();
 
@@ -105,56 +148,17 @@ public class Schedule {
             // add some randomness aka people coming/leaving early/late
 
             // lets people come around quarter past the hour +- 10 mins
-            double time = lecture.time + 0.25 + (rng.nextDouble() - 0.5) * 0.33;
+            double time = lecture.time + QUARTER_HOUR + (rng.nextDouble() - 0.5) * TWENTY_MINS;
 
             // lets people leave around quarter to the hour +- 10 mins
-            double duration = lecture.duration - 0.5 + (rng.nextDouble() - 0.5) * 0.33;
+            double duration = lecture.duration - HALF_HOUR + (rng.nextDouble() - 0.5) * TWENTY_MINS;
 
             slots.add(new ScheduleSlot(time, duration, lecture.room));
         }
 
         slots.sort(Comparator.comparingDouble(o -> o.time));
 
-        // second set lunch slot
-
-        double lunchWindow = 1;
-        pickLunchSlot: while (lunchWindow < 10) {
-
-            double lunchTime = 12 + (rng.nextDouble()-0.5) * lunchWindow;
-
-            // getting lunch takes 10 to 20 mins
-            double lunchWaitDuration = 0.166 + rng.nextDouble() * 0.166;
-
-            // eating lunch takes 20 to 40 mins
-            double lunchEatDuration = 0.33 + rng.nextDouble() * 0.33;
-
-            double lunchEndTime = lunchTime + lunchWaitDuration + lunchEatDuration;
-
-            lunchWindow += 1;
-
-            for (int j = 0; j < slots.size(); j++) {
-                ScheduleSlot slot = slots.get(j);
-                // try again if overlaps with other lecture
-                if (lunchTime <= slot.time && lunchEndTime >= slot.time + slot.duration) {
-                    continue pickLunchSlot;
-                }
-            }
-
-            // getting lunch
-            RoomBase.RoomType lunchRoom = RoomBase.GetRandomLunchOption();
-            slots.add(new ScheduleSlot(lunchTime, lunchWaitDuration, lunchRoom));
-
-
-            // eating lunch
-            RoomBase.RoomType eatRoom = RoomBase.GetRandomGatheringRoom();
-            slots.add(new ScheduleSlot(lunchTime + lunchWaitDuration, lunchEatDuration, eatRoom));
-
-            break;
-        }
-
-        slots.sort(Comparator.comparingDouble(o -> o.time));
-
-        // third fill slots between lectures / lunch
+        // second fill slots between lectures
         int i = 0;
         while (i < slots.size()-1) {
 
@@ -164,37 +168,43 @@ public class Schedule {
             double start = l1.time + l1.duration;
             double gap = l2.time - start;
 
-            // add some seminars / study time between lectures
-            if (gap >= 0.7 && rng.nextInt(3) > 1) {
-
-                double studyTime = start + rng.nextDouble() * 0.166;
-
-                // 30 mins to all available time
-                double studyDuration = 0.5 + rng.nextDouble() * (gap - 0.5 - (studyTime-start));
-
-                if (studyTime > start) {
-                    // just hangout before
-                    RoomBase.RoomType room = RoomBase.GetRandomGatheringRoom();
-                    slots.add(++i, new ScheduleSlot(start, studyTime - start, room));
-                }
-
-                // seminars or study time are just somewhere in a wing
-                RoomBase.RoomType wing = RoomBase.GetRandomWing();
-                slots.add(++i, new ScheduleSlot(studyTime, studyDuration, wing));
-
-                if (studyTime + studyDuration < l2.time) {
-                    // just hangout after
-                    RoomBase.RoomType room = RoomBase.GetRandomGatheringRoom();
-                    slots.add(++i, new ScheduleSlot(studyTime + studyDuration, l2.time - studyTime - studyDuration, room));
-                }
-
-            } else {
-                // just hangout for the available time
-                RoomBase.RoomType room = RoomBase.GetRandomGatheringRoom();
-                slots.add(++i, new ScheduleSlot(start, gap, room));
+            if (gap == 0) {
+                i++;
+                continue;
             }
 
-            i++;
+            double choice = rng.nextDouble();
+
+            if (choice <= STUDENT_PROB_EAT && gap >= FIVE_MINS) {
+                // choice 1: get something to eat/drink
+
+                // getting lunch takes between 5 and 15 mins
+                double duration = durationBetween(rng, FIVE_MINS, QUARTER_HOUR, gap);
+
+                RoomBase.RoomType foodRoom = RoomBase.GetRandomLunchOption();
+
+                slots.add(++i, new ScheduleSlot(start, duration, foodRoom));
+
+            } else if (choice <= STUDENT_PROB_EAT + STUDENT_PROB_CHAIR && gap >= QUARTER_HOUR) {
+                // choice 2: go to a chair for a seminar or study
+
+                // between 15 mins and 2 hours
+                double duration = durationBetween(rng, QUARTER_HOUR, 2, gap);
+
+                RoomBase.RoomType seminarRoom = RoomBase.GetRandomWing();
+
+                slots.add(++i, new ScheduleSlot(start, duration, seminarRoom));
+
+            } else {
+                // choice 3: hang out somewhere
+
+                // between 10 mins and 1 hour
+                double duration = durationBetween(rng, 10, 1, gap);
+
+                RoomBase.RoomType room = RoomBase.GetRandomGatheringRoom();
+
+                slots.add(++i, new ScheduleSlot(start, duration, room));
+            }
         }
 
         System.out.println("Student Schedule "+seed+": "+slots);
@@ -203,7 +213,7 @@ public class Schedule {
     }
 
     public static Schedule forProfessor(int seed) {
-        Random rng = new Random(seed);
+        Random rng = randomForSeed(seed);
 
         ArrayList<ScheduleSlot> slots = new ArrayList<>();
 
@@ -236,18 +246,16 @@ public class Schedule {
 
             // add some randomness - professors always come early and leave late
 
-            // lets people come around quarter past the hour - 10 mins
+            // lets professors come around quarter past the hour - 10 mins
             double time = lecture.time + 0.25 - rng.nextDouble() * 0.1;
 
-            // lets people leave around quarter to the hour + 10 mins
+            // lets professors leave around quarter to the hour + 10 mins
             double duration = lecture.duration - 0.5 + rng.nextDouble() * 0.1;
 
             slots.add(new ScheduleSlot(time, duration, lecture.room));
         }
 
         slots.sort(Comparator.comparingDouble(o -> o.time));
-
-        boolean hadLunch = false;
 
         // the home chair of the professor
         RoomBase.RoomType chair = RoomBase.GetRandomWing();
@@ -262,24 +270,42 @@ public class Schedule {
             double start = l1.time + l1.duration;
             double gap = l2.time - start;
 
-            // go for lunch only if at least 20 mins time and after 10:30
+            i++;
 
-            if (!hadLunch && start >= 10.5 && gap > 0.2) {
-                double gettingLunchDuration = 0.1;
+            if (gap == 0) {
+                continue;
+            }
 
-                // getting lunch (10 mins)
-                RoomBase.RoomType room = RoomBase.GetRandomLunchOption();
-                slots.add(++i, new ScheduleSlot(start, gettingLunchDuration, room));
-                hadLunch = true;
+            double choice = rng.nextDouble();
 
-                // eating lunch at chair (rest available time)
-                slots.add(++i, new ScheduleSlot(start + gettingLunchDuration, gap - gettingLunchDuration, chair));
+            if (choice <= PROF_PROB_EAT && gap >= FIVE_MINS) {
+                // choice 1: get something to eat/drink
+
+                // getting lunch takes between 5 and 15 mins
+                double duration = durationBetween(rng, FIVE_MINS, QUARTER_HOUR, gap);
+
+                RoomBase.RoomType foodRoom = RoomBase.GetRandomLunchOption();
+
+                slots.add(i, new ScheduleSlot(start, duration, foodRoom));
+
+            } else if (choice <= PROF_PROB_EAT + PROF_PROB_HANGOUT && gap >= FIVE_MINS) {
+                // choice 2: hang out somewhere
+
+                // between 5 and 30 mins
+                double duration =durationBetween(rng,  FIVE_MINS, HALF_HOUR, gap);
+
+                RoomBase.RoomType room = RoomBase.GetRandomGatheringRoom();
+
+                slots.add(i, new ScheduleSlot(start, duration, room));
 
             } else {
-                // be in the chair for the available time
-                slots.add(++i, new ScheduleSlot(start, gap, chair));
+                // choice 3: be in the chair
+
+                // between 10 mins and 1 hour
+                double duration = durationBetween(rng, 10, 1, gap);
+
+                slots.add(i, new ScheduleSlot(start, duration, chair));
             }
-            i++;
         }
 
         System.out.println("Professor Schedule "+seed+": "+slots);
@@ -289,17 +315,17 @@ public class Schedule {
 
 
     public static Schedule forBarista(int seed) {
-        Random rng = new Random(seed);
+        Random rng = randomForSeed(seed);
 
         ArrayList<ScheduleSlot> slots = new ArrayList<>();
 
         RoomBase.RoomType workplace = RoomBase.GetRandomLunchOption();
 
-        // Somewhere between 8 and 12
+        // Somewhere between 8 and 14
         double shiftStart = START_OF_DAY + rng.nextDouble()*4;
 
-        // Somewhere between 4 and 7 hours
-        double shiftDuration = 4 + rng.nextDouble()*3;
+        // Somewhere between 4 and 8 hours
+        double shiftDuration = Math.min(4 + rng.nextDouble()*4, END_OF_DAY - shiftStart - 1);
 
         // Somewhere in the middle of the shift
         double breakStart = shiftStart + shiftDuration/2 + (rng.nextDouble()-0.5);
@@ -319,6 +345,114 @@ public class Schedule {
         System.out.println("Barista Schedule "+seed+": "+slots);
 
         return new Schedule(slots);
+    }
+
+    public static Schedule forVisitor(int seed) {
+        Random rng = randomForSeed(seed);
+
+        ArrayList<ScheduleSlot> slots = new ArrayList<>();
+
+        // Somewhere between 8 and 18
+        double visitStart = START_OF_DAY + rng.nextDouble()*10;
+
+        // Somewhere between 2 and 6 hours
+        double visitDuration = Math.min(2 + rng.nextDouble()*4, END_OF_DAY - visitStart - 1);
+
+        double visitEnd = visitStart + visitDuration;
+
+        double time = visitStart;
+
+        while (time < visitEnd) {
+
+            double gap = visitEnd - time;
+
+            double choice = rng.nextDouble();
+
+            if (choice <= VISITOR_PROB_EAT && gap >= FIVE_MINS) {
+                // choice 1: get something to eat/drink
+
+                // getting lunch takes between 5 and 15 mins
+                double duration =durationBetween(rng,  FIVE_MINS , QUARTER_HOUR, gap);
+
+                RoomBase.RoomType foodRoom = RoomBase.GetRandomLunchOption();
+
+                slots.add(new ScheduleSlot(time, duration, foodRoom));
+                time += duration;
+
+            } else if (choice <= VISITOR_PROB_EAT + VISITOR_PROB_CHAIR && gap >= TEN_MINS) {
+                // choice 2: visit a chair
+
+                // between 10 and 30 mins
+                double duration =durationBetween(rng,  TEN_MINS, HALF_HOUR, gap);
+
+                RoomBase.RoomType chair = RoomBase.GetRandomWing();
+
+                slots.add(new ScheduleSlot(time, duration, chair));
+            } else {
+                // choice 2: hang out somewhere
+
+                // between 10 and 30 mins
+                double duration = durationBetween(rng, 10,  HALF_HOUR, gap);
+
+                RoomBase.RoomType room = RoomBase.GetRandomGatheringRoom();
+
+                slots.add(new ScheduleSlot(time, duration, room));
+                time += duration;
+
+            }
+
+        }
+
+        System.out.println("Visitor Schedule "+seed+": "+slots);
+
+        return new Schedule(slots);
+    }
+
+    public static Schedule forCleaner(int seed) {
+        Random rng = randomForSeed(seed);
+
+        ArrayList<ScheduleSlot> slots = new ArrayList<>();
+
+        // early shift: between 8 and 10
+        // late shift: between 18 and 20;
+        boolean earlyShift = rng.nextInt(100) % 2 == 0;
+
+        double shiftStart = earlyShift ? START_OF_DAY + rng.nextDouble()*QUARTER_HOUR : END_OF_DAY - 2 - rng.nextDouble()*QUARTER_HOUR;
+
+        double shiftEnd = earlyShift ? START_OF_DAY + 2 + rng.nextDouble()*QUARTER_HOUR : END_OF_DAY - FIVE_MINS - rng.nextDouble()*QUARTER_HOUR;
+
+        double time = shiftStart;
+        RoomBase room = RoomBase.AllRooms.get(RoomBase.RoomType.Magistrale);
+
+        // go through neighboring rooms randomly
+        while (time < shiftEnd) {
+            double gap = shiftEnd - time;
+
+            double duration = durationBetween(rng, FIVE_MINS, HALF_HOUR, gap);
+
+            slots.add(new ScheduleSlot(time, duration, room.GetRoomType()));
+
+            time += duration;
+            room = room.GetRandomNeighboringRoom();
+        }
+
+        System.out.println("CLEANER Schedule "+seed+": "+slots);
+
+        return new Schedule(slots);
+    }
+
+    private static double durationBetween(Random rng, double low, double high, double max) {
+        double duration = low + rng.nextDouble() * high;
+        return Math.min(duration, max);
+    }
+
+    private static Random randomForSeed(int seed) {
+        Random rng = new Random(seed * 12345);
+
+        // weird fix since the first value appears to be less random
+        rng.nextDouble();
+
+        return rng;
     }
 
     public RoomBase getNextRoom(double currentTime) {
@@ -362,7 +496,7 @@ public class Schedule {
             }
         }
 
-        return Double.MAX_VALUE;
+        return (END_OF_DAY - START_OF_DAY) * 3600;
     }
 }
 
